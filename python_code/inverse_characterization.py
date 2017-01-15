fa87cb9a2f66dd58fa4b64b210bbc1565bcf1ed9
José Pedro <1jpsneto@gmail.com>
2017-01-15 15:32:35 -0200

    Adding code working well

--- python_code/inverse_characterization.py
+++ python_code/inverse_characterization.py
@@ -1,172 +1,116 @@
+# inversse characterization
 # -*- coding: utf-8 -*-
 from scipy.optimize import differential_evolution
-import numpy
+import numpy as np
 import scipy.io
-import math
-
+#import math
+import matplotlib.pyplot as plt
 '''
 #best1exp
 #f = 0.935
 #c_r = 1
-#tamanho_populacao = 100
+#tamanho_populacao = 40
 '''
+#%%
+def vector_frequencies(fmax,fmin,deltaf):
 
-def ackley(x):
-	arg1 = -0.2 * numpy.sqrt(0.5 * (x[0] ** 2 + x[1] ** 2))
-	arg2 = 0.5 * (numpy.cos(2. * numpy.pi * x[0]) + numpy.cos(2. * numpy.pi * x[1]))
-	return -20. * numpy.exp(arg1) - numpy.exp(arg2) + 20. + numpy.e
-
-def objetivo_allard_limp_funcao_dupla(vector):
-    params_allard_rigido_simples = allard_rigido(0.025,2000,vector[0],vector[1],vector[2],vector[3],vector[4])
-    Zs1 = params_allard_rigido_simples['Zs']
-    params_allard_rigido_duplo = allard_rigido(2*0.025,2000,vector[0],vector[1],vector[2],vector[3], vector[4])
-    Zs2 = params_allard_rigido_duplo['Zs']
-
-    mat = scipy.io.loadmat('Z_exp.mat')
+    f = np.arange(fmin,fmax+deltaf,deltaf)
     
-    Z_referencia_esp1 = mat['Z_3']
-    F_obj_1 = (numpy.absolute(numpy.subtract((Z_referencia_esp1[500-1:1650-1]/413),(Zs1[500-1:1650-1]/413) ) )**2)
+    omega=2*np.pi*f
+    return [f,omega]
+        
+#%%
+def objetivo_allard_limp_funcao_dupla(x):
+    params_JC_simples = m_Johnson_Champoux3_deltaf(fmax,fmin,deltaf,espessura1,x[0],x[1],x[2],x[3],x[4],x[5],structure='rigid')
+    A1 = params_JC_simples['alpha']
+    params_JC_duplo = m_Johnson_Champoux3_deltaf(fmax,fmin,deltaf,espessura2,x[0],x[1],x[2],x[3],x[4],x[5],structure='rigid')
+    A2 = params_JC_duplo['alpha']
+
+
+    F_obj_1 = np.absolute(A_referencia_esp1[600:5000+1]-A1[600:5000+1])**2
+#    F_obj_1 = np.absolute(Z_referencia_esp1[500:1650+1]/413-Zs1[500:1650+1]/413)**2
     F_obj_1 = F_obj_1.sum()
 
-    Z_referencia_esp2 = mat['Z_6']
-    F_obj_2 = (numpy.absolute(numpy.subtract((Z_referencia_esp2[500-1:1650-1]/413),(Zs2[500-1:1650-1]/413) ) )**2)
+
+    F_obj_2 = np.absolute(A_referencia_esp2[600:5000+1]-A2[600:5000+1])**2
+#    F_obj_2 = np.absolute(Z_referencia_esp2[500:1650+1]/413-Zs2[500:1650+1]/413)**2
     F_obj_2 = F_obj_2.sum()
 
-    print F_obj_1
-    print F_obj_2
-
-    F_obj = 3*F_obj_1 + F_obj_2
-
-    #print 'F_obj'
-    #print F_obj
-
+    F_obj = F_obj_1 + F_obj_2
+ 
     return F_obj
 
-def allard_rigido(L, fmax, sigma, phi, alpha_inf, Lambda_material, fator):
-    '''
-    ## Modelo de material poroso de estrutura rigido
-    # Calcula densidade efetiva dinamica e compressibilidade efetiva para
-    # modelos de fluido equivalente de Johnson-Lafarge. 
-    # [Zs alpha] = Lafarge(L,freq,sigma,phi,alpha_inf,Lambda_material,Lambda_l_material)
-    # Paramentros de entrada:
-    # L = comprimento da amostra;
-    # freq = frequencia maxima;
-    # sigma = resistividade ao fluxo [Ns/m^2];
-    # phi = porosidade [%]
-    # alpha_inf = tortuosidade (normalmente varia de 1 a 4)
-    # Lambda_material = Comprimento caracteristico viscoso
-    # Lambda_l_material = Comprimento caracteristico termico
+#%%
+def m_Johnson_Champoux3_deltaf(fmax,fmin,deltaf,espessura,sigma,phi,alpha_inf,lambda_v,fator,rho,structure='rigid'):
+           
+    rho_ef_jc = rho0*alpha_inf*(1+((phi*sigma)/(1j*rho0*alpha_inf*omega))*((1+1j*((4*eta0*rho0*alpha_inf**2*omega)/(sigma**2*phi**2*lambda_v**2)))**(1./2.)))
+    rho_eq_jc=rho_ef_jc
 
-    # Como saida tem-se:
-    # Zs = impedancia de superficie
-    # Alpha = Coeficiente de absorcao
-    '''
+    if structure=='limp':
+        rho_eq_jc = (rho_eq_jc*rho-rho0**2)/(rho+rho_eq_jc-2*rho0)
 
-    L_material = L; # espessura do material poroso
-    rho_0 = 1.204;      # Densidade [kg/m^3]
-    T = 20;               # Temperatura
-    c_0 = (331.2+0.6*T);# velocidade de propagacao no meio [m/s]
-    eta = 1.84e-5;      # Coeficiente de viscosidade, viscosidade absoluta ou viscosidade dinamica
-    gamma = 1.4;        # Razão de calores específicos (gamma=c_p/c_v)
-    k_f = 0.026;        # Condutibilidade térmica do fluido [W/mK]
-    P_0 = 101320;       # Pressão estática do meio [Pa]
-    c_p = 1.0035e3;     # Coeficiente de Calor específico a pressão constante (c_p), (c_v é coef. calor esp. a VOLUME cte)
-    Pr = (eta*c_p)/k_f;   # Número de Prandtl (???)--- c_p calor específico, k_f é
-
-    # Parâmetros Macro
-    sigma_material = sigma;      # Resistividade ao fluxo [kN/s]
-    phi_material =  phi;         # Porosidade 60#
-    alpha_inf_material = alpha_inf;    # Tortuosidade
-    Lambda = Lambda_material;
-    #fator = 1
-    Lambda_l = Lambda_material*fator 
-    #Lambda_l = Lambda_l_material;
-    q_0 = eta/sigma;
-    # q_l_0 = q_0*Lambda_l/Lambda;
-    q_l_0 = (phi_material*Lambda_l*Lambda_l)/8.;
-
-    f = numpy.arange(0, fmax, 1)
-
-    w = 2*math.pi*f ## MODELO DE Johnson - Lafarge
-
-    D = (eta*(phi_material**2)*(Lambda**2));
-    C = (4*w*rho_0*(q_0**2)*(alpha_inf_material**2));
-    #numpy.divide(a,b,dtype=float)
-    G = (1j*w*rho_0*alpha_inf_material*q_0)
-    G[G==0] = numpy.finfo(float).eps
-    B = numpy.divide((phi_material*eta),G)
-    #B = ((phi_material*eta)/(1j*w*rho_0*alpha_inf_material*q_0));
-    E = ( 1 + 1j*numpy.divide(C, D));
-    E = numpy.power(E,1/2)
-    A = 1 + numpy.multiply(B, E)
-    rho_rigido = rho_0*alpha_inf_material * A; # densidade dinâmica efetiva
-
-    print 'rho_rigido'
-    print numpy.mean(rho_rigido)
-
-    F = eta*(phi_material**2)*(Lambda_l**2)
-    E = numpy.multiply(4*rho_0*Pr*(q_l_0**2), w)
-    D =  1 + (1j* numpy.divide(E, F))
-    G = (1j*w*rho_0*Pr*q_l_0)
-    G[G==0] = numpy.finfo(float).eps
-    C = numpy.divide((phi_material*eta), G)
-    # C e D tao errados
-    B = 1 + numpy.multiply(C, numpy.power(D,(1/2)))
-    A = numpy.divide(gamma-(gamma-1), B)
-    K_ef = numpy.divide(gamma*P_0, A) # módulo de compressibilidade 
-    print 'B'
-    print numpy.mean(B)
-
-    #print 'compressibilidade'
-    #print numpy.mean(K_ef)
-
-    Zc = numpy.sqrt(numpy.multiply(rho_rigido, K_ef))
-    A = numpy.divide(rho_rigido, K_ef)
-    B = numpy.power(A,(1/2))
-    kc_L = numpy.multiply(w, B)
-    A = numpy.multiply(kc_L, L_material)
-    H = numpy.multiply(phi_material,numpy.arctan(A))
-    H[H==0] = numpy.finfo(float).eps
-    Zs = -1j*numpy.divide(Zc, H)
+    K_ef_jc = gamma*P0/(gamma-((gamma-1)*(1+((8*eta0)/(1j*omega*Pr*(fator*lambda_v)**2*rho0))*(1+1j*((omega*Pr*rho0*(fator*lambda_v)**2)/(16*eta0)))**(1./2.))**(-1)))
     
-    alpha = 1 - numpy.power((numpy.absolute(numpy.divide((Zs - rho_0*c_0),(Zs + rho_0*c_0)))), 2);
+    
+    k=omega*(rho_eq_jc/K_ef_jc)**(1./2.)
+    Zc=(rho_eq_jc*K_ef_jc)**(1./2.)
+    
+    Zs=-(1j/phi)*Zc*(1/np.tan(k*espessura))
+    R=(Zs-rho0*c0)/(Zs+rho0*c0)
+    Abs=1-np.absolute(R)**2
 
-    params_allard_rigido = {'Zs': Zs, 'alpha': alpha}
+    params_JC = {'Zs': Zs, 'alpha': Abs}
+    
+    print fator
 
-    return params_allard_rigido
+    return params_JC
+    
 
-#bounds = [(-5, 5), (-5, 5)]
-# Lambda_material = Comprimento característico viscoso
-# Lambda_l_material = Comprimento característico térmico
-# Lambda_l = Lambda_l_material;
-# Lambda_l = Lambda_material*fator
-# Comprimento característico térmico = (Comprimento característico viscoso)*fator
-'''
-paramDefCell = {'parameter1', [5000 300000], 10     # RESISITIVDADE AO FLUXO
-            'parameter2', [0.90   0.99], 0.01   # POROSIDADE
-            'parameter3', [1 4], 0.01           # TORTUOSIDADE
-            'parameter4', [10e-6 500e-6], 1e-6  # COMRPIMENTO CARACTERÍSTICO VISCOSO
-            'parameter5', [10e-6 500e-6], 1e-6  # COMPRIMENTO CARACTERÍSTICO TÉRMICO ou FATOR (1 3)
-            'parameter6', (64, 64), 0.01 };      # DENSIDADE
-'''
+if __name__ == "__main__":
 
-#bounds = [ (5000, 300000), (0.90, 0.99), (1, 4), (10e-6, 500e-6), (1,3), (64, 64)]
-#result = differential_evolution(ackley, bounds, strategy='best1exp', disp=True, popsize=100, mutation=0.935, recombination=1)
+	#Air properties
+	P0 = 101325.0                   #static pressure [Pa]
+	R_spec = 287.058                #specific gas constant [J/(kg·K)]
+	T = 20.0                        #temperature [°C]
+	rho0 = P0/(R_spec*(T+273.15))      #air density [kg/m^3] (ideal gas law)
+	c0 = 331.3+0.606*T                  #speed of sound in dry air [m/s]
+	eta0 = ((1.458e-6)*(T+273.15)**(3./2.))/((T+273.15)+110.4)    #dynamic viscosity [kg/(m*s)] (Sutherland's law)
+	k_f = ((2.334e-3)*(T+273.15)**(3./2.))/((T+273.15)+164.54)      #thermal conductivity [W/(m*K)]
+	gamma = 1.4                         #heat capacity ratio (ratio of specific heats)
+	c_p = 1.0035e3                      #specific heat capacity at constant pressure [J/(kg*K)]
+	Pr = eta0*c_p/k_f                   #Prandtl number
 
-#print result.x 
-#print result.fun
+	# Setup parameters
+	fmax=5500
+	fmin=0
+	deltaf=1
+	[f,omega]=vector_frequencies(fmax,fmin,deltaf)
+	espessura1=0.024
+	espessura2=2*0.024
 
-vector = numpy.array([24930, 0.91, 1, 4.1000e-05, 2.847222222222222, 64])
-f_obj = objetivo_allard_limp_funcao_dupla(vector)
+	simples = scipy.io.loadmat('../data/espuma_preta/espumapreta_24mm_simples.mat')
+	duplo = scipy.io.loadmat('../data/espuma_preta/espumapreta_48mm_duplo.mat')
+	A_referencia_esp1 = simples['A_A1']
+	A_referencia_esp1=np.ravel(A_referencia_esp1)
+	A_referencia_esp2 = duplo['A_A1']
+	A_referencia_esp2=np.ravel(A_referencia_esp2)
 
-print f_obj
 
-'''
-compressibilidade da errado
+	bounds = [ (5000, 300000), (0.80, 0.99), (1, 4), (10e-6, 500e-6), (1,6), (64, 64)]
+	result = differential_evolution(objetivo_allard_limp_funcao_dupla, bounds, strategy='best1exp', disp=True, popsize=20, mutation=0.935, recombination=1)
 
-compressibilidade
-(5.34825643657e+12-116.617165994e+04j)
+	print result.x
+	print result.fun
 
-1.1110e+05 + 1.1826e+04i
+	params_JC_otim1=m_Johnson_Champoux3_deltaf(fmax,fmin,deltaf,espessura1,result.x[0],result.x[1],result.x[2],result.x[3],result.x[4],result.x[5],structure='rigid')
+	params_JC_otim2=m_Johnson_Champoux3_deltaf(fmax,fmin,deltaf,espessura2,result.x[0],result.x[1],result.x[2],result.x[3],result.x[4],result.x[5],structure='rigid')
+	alpha_otim1=params_JC_otim1['alpha']
+	alpha_otim2=params_JC_otim2['alpha']
 
-'''
+
+	plt.figure(1)
+	plt.plot(f[200:5500+1],A_referencia_esp1[200:5500+1],f[200:5500+1],A_referencia_esp2[200:5500+1],f[200:5500+1],alpha_otim1[200:5500+1],f[200:5500+1],alpha_otim2[200:5500+1])
+	plt.xlabel('frequency')
+	plt.ylabel('alpha')
+	plt.grid()
+	plt.show()
---
+++ python_code/optimize.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 09 10:16:08 2017
+
+@author: Thiago and Jose Pedro
+"""
+
+# -*- coding: utf-8 -*-
